d77d92b9a38f68e3823c549467e61db0af9ece83
Charlene-ai <159562463+Charlene-ai@users.noreply.github.com>
2024-02-09 10:12:13 -0600

    Update speech.js

--- speech.js
+++ speech.js
@@ -1,14 +1,6 @@
 function speak() {
     var text = document.getElementById('text').value;
-    var lang = document.getElementById('lang').value;
-    var volume = document.getElementById('volume').value;
-    var rate = document.getElementById('rate').value;
-
     var msg = new SpeechSynthesisUtterance();
     msg.text = text;
-    msg.lang = lang;
-    msg.volume = volume;
-    msg.rate = rate;
-
     window.speechSynthesis.speak(msg);
 }
